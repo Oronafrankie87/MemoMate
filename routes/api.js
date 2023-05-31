cfc468deb39a2161c66d206796f51346c0f9ff84
Francisco Orona <oronafrankie87@gmail.com>
2023-05-30 21:43:47 -0600

    api.js done

--- routes/api.js
+++ routes/api.js
@@ -13,3 +13,21 @@ router.get("/notes", (req, res) => {
   res.json(JSON.parse(db));
 });
 //The code above ^ sets up a route to handle GET requests to the notes endpoint.  When the endpoint is accessed, it reads the contents of a JSON file, parses it into an object and sends the object as the response in JSON format.
+
+
+router.post('/notes', (req, res) => {
+  const dbPath = path.join(__dirname, './db/db.json');
+  const db = JSON.parse(fs.readFileSync(dbPath));
+  const userNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uniqid(),
+  };
+  db.push(userNote);
+  fs.writeFileSync(dbPath, JSON.stringify(db));
+  res.json(db);
+});
+//The code above ^ sets up a route to handle POST requests to the notes endpoint.  When a POST request is made, it reads the existing notes from a JSON file, adds a new note based on the request body, updates the JSON file with the new note, and sends the updated list of notes as the response.
+
+module.exports = router;
+//This code ^ exports the router object, making it available for other modules to use
